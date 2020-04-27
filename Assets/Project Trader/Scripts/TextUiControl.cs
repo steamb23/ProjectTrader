47d42716f81a32811a56d33b22f11c722cfcd806
BuildTools <unconfigured@null.spigotmc.org>
2020-04-27 17:06:46 +0900

    텍스트출력UI / 시간관리 코드추가 + sample 텍스트박스prefab

---
+++ Assets/Project Trader/Scripts/TextUiControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextUiControl : MonoBehaviour
+{
+    //textbox프리팹 적용(이미지-텍스트)
+    public GameObject[] textBox;
+
+    GameObject canvas;
+    RectTransform tbPos;
+
+    //텍스트가 나타났다 사라지는 시간
+    //public int textBoxTime=2;
+
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+        tbPos = GetComponent<RectTransform>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+    // 말풍선의 종류, x위치,y위치,문자열+유지시간 추가가능,유지시간
+    public void CreativeTextBox(int a, int x, int y, string gg, int ti)
+    {
+        GameObject te = Instantiate(textBox[a]) as GameObject;
+        TextMeshProUGUI text_re = te.GetComponentInChildren<TextMeshProUGUI>();
+
+        //string적용
+        text_re.text = gg;
+
+        //캔버스에 생성
+        te.transform.SetParent(canvas.transform);
+        tbPos = te.GetComponent<RectTransform>();
+
+        //위치조절
+        tbPos.anchoredPosition = new Vector3(x, y);
+
+        //제거
+        Destroy(te, ti);
+    }
+}
