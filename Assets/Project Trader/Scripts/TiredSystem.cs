196f71d5cc682264ab20638dcb0cd785022d949c
soyeon <lsy23904@gmail.com>
2020-04-30 17:37:10 +0900

    중간 커밋

---
+++ Assets/Project Trader/Scripts/TiredSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiredSystem : MonoBehaviour
+{
+
+    public int maxHeart;
+    public int dayTired;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
