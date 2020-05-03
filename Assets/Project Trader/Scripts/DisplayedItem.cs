0c8cfc9345ce2dce729dcb1d3305aa868f630ea6
SteamB23 <steamb23@outlook.com>
2020-05-03 20:34:42 +0900

    DisplayedItem 및 ItemNode 일부 구현

---
+++ Assets/Project Trader/Scripts/DisplayedItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 진열된 아이템을 나타냅니다.
+/// </summary>
+public class DisplayedItem : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    // TODO: 아이템 데이터, 추가 예정
+    // public Item itemData
+
+    /// <summary>
+    /// 남은 아이템. 인스펙터 전용. 코드에서 접근하지마세요.
+    /// </summary>
+    [Obsolete("인스펙터 전용 변수입니다. 대신에 ItemCount 프로퍼티를 사용하세요.")]
+    public int itemCount;
+
+#pragma warning disable CS0618
+    public int ItemCount
+    {
+        get => itemCount;
+        set
+        {
+            itemCount = value;
+
+            // 스프라이트 표시 변경
+            spriteRenderer.enabled = itemCount > 0;
+        }
+    }
+#pragma warning restore CS0618
+
+    void Start()
+    {
+        // null 이면 SpriteRenderer를 검색
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null)
+                spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        // itemData가 null이 아니면 해당 데이터로 스프라이트 교체
+
+        // 스프라이트 표시 여부 초기화
+        ItemCount = ItemCount;
+    }
+}
---
+++ Assets/Project Trader/Scripts/PathNodes/ItemNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemNode : PathNode
+{
+    public DisplayedItem displayedItem;
+
+    void Start()
+    {
+        if (displayedItem == null)
+            Debug.LogWarning($"{name}의 displayedItem 항목에 아무런 오브젝트도 연결되어 있지 않습니다!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
