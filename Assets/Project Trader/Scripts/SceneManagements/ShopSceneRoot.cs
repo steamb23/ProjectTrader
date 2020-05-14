cb38c37a174e45f9da4e3c87c850633f2815a86d
SteamB23 <steamb23@outlook.com>
2020-05-14 17:19:07 +0900

    GameScene과 ShopScene 분리

---
+++ Assets/Project Trader/Scripts/SceneManagements/ShopSceneRoot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopSceneRoot : MonoBehaviour
+{
+    //private void Awake()
+    //{
+    //    // 패스 파인더 재스캔 (씬 로딩시 서브씬의 충돌체를 감지하지 못해서 강제로 스캔)
+    //    AstarPath pathfinder = FindObjectOfType<AstarPath>();
+    //    pathfinder.Scan();
+    //}
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
