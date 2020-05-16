8f459e32d965874cc14491fe44f7b1d66f50eb86
SteamB23 <steamb23@outlook.com>
2020-05-17 02:05:33 +0900

    손님 스프라이트 데이터 추가
    대기 모션

--- Assets/Project Trader/Classes/SpriteDatas/VisitorSpriteData.cs
+++ Assets/Project Trader/Classes/SpriteDatas/VisitorSpriteData.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using Unity.Collections;
+using System.Collections.ObjectModel;
 
 namespace ProjectTrader.SpriteDatas
 {
@@ -10,25 +11,29 @@ namespace ProjectTrader.SpriteDatas
     public class VisitorSpriteData : ScriptableObject
     {
         [Serializable]
+        public class VisitorSpriteDictionary : SerializableDictionary<string, ActionData> { }
+
+        [SerializeField]
+        VisitorSpriteDictionary dictionary;
+
+        public VisitorSpriteDictionary Dictionary => dictionary;
+
+        [Serializable]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235")]
         public struct Data
         {
-            public Sprite[] front;
-            public Sprite[] back;
-            public Sprite[] left;
-            public Sprite[] right;
+            public Sprite[] Front;
+            public Sprite[] Back;
+            public Sprite[] Left;
+            public Sprite[] Right;
         }
 
-        [SerializeField]
-        private Data idle;
-        [SerializeField]
-        private Data walk;
-        [SerializeField]
-        private Data grab;
-
-        public Data Idle => idle;
-        public Data Walk => walk;
-        public Data Grab => grab;
+        [Serializable]
+        public struct ActionData
+        {
+            public Data Idle;
+            public Data Walk;
+            public Data Grab;
+        }
     }
-
 }
---
+++ Assets/Project Trader/Editor/AnySerializableDictionaryPropertyDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace ProjectTrader.Editors
+{
+    [CustomPropertyDrawer(typeof(SpriteDatas.VisitorSpriteData.VisitorSpriteDictionary))]
+    public class AnySerializableDictionaryPropertyDrawer : SerializableDictionaryPropertyDrawer
+    {
+    }
+}
