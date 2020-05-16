b2b644ff82bedad7773f6e0adb00bc2c1a7a2ea5
SteamB23 <steamb23@outlook.com>
2020-05-17 03:20:14 +0900

    재료 데이터 불러오기 추가

--- Assets/Project Trader/Classes/IngameDatabase.cs
+++ Assets/Project Trader/Classes/IngameDatabase.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace ProjectTrader.Datas
 {
-    
+
     public static class IngameDatabase
     {
         public class ReadOnlyContainer<T>
@@ -40,18 +40,64 @@ namespace ProjectTrader.Datas
 
         public static void Initialize()
         {
-            var itemCsvData = CsvData.LoadFromResources("Datas/ItemData");
-            ItemData[] itemDatas = new ItemData[itemCsvData.RowLength];
-            for(int i = 1; i < itemDatas.Length; i++)
-            {
-                ItemData itemData = new ItemData();
-                itemData.Code = Convert.ToInt32(itemCsvData[i, "CODE"]);
-                itemData.SellPrice = Convert.ToInt32(itemCsvData[i, "SELL_PRICE"]);
-
-                itemDatas[i] = itemData;
-            }
 #pragma warning disable CS0618 // 형식 또는 멤버는 사용되지 않습니다.
-            Set(itemDatas);
+            CsvData csvData;
+            #region 아이템 데이터 초기화
+            {
+                csvData = CsvData.LoadFromResources("Datas/ItemData");
+                ItemData[] datas = new ItemData[csvData.RowLength];
+                for (int i = 1; i < datas.Length; i++)
+                {
+                    // 재료 데이터 배열 초기화
+                    int[] materialCodes;
+                    {
+                        var raw = csvData[i, "MATERIAL_CODE"];
+                        var rawSplit = raw.Split('/');
+                        materialCodes = new int[rawSplit.Length];
+                        for (int x = 0; x < rawSplit.Length; x++)
+                        {
+                            materialCodes[x] = Convert.ToInt32(rawSplit[x]);
+                        }
+                    }
+                    int[] materialNeeds;
+                    {
+                        var raw = csvData[i, "MATERIAL_NEEDS"];
+                        var rawSplit = raw.Split('/');
+                        materialNeeds = new int[rawSplit.Length];
+                        for (int x = 0; x < rawSplit.Length; x++)
+                        {
+                            materialNeeds[x] = Convert.ToInt32(rawSplit[x]);
+                        }
+                    }
+
+                    ItemData data = new ItemData();
+                    data.Code = Convert.ToInt32(csvData[i, "CODE"]);
+                    data.Name = Convert.ToString(csvData[i, "NAME"]);
+                    data.SellPrice = Convert.ToInt32(csvData[i, "SELL_PRICE"]);
+                    data.MaterialCodes = materialCodes;
+                    data.MaterialNeeds = materialNeeds;
+
+                    datas[i] = data;
+                }
+                Set(datas);
+            }
+            #endregion
+
+            #region 재료 데이터 초기화
+            {
+                csvData = CsvData.LoadFromResources("Datas/MaterialData");
+                MaterialData[] datas = new MaterialData[csvData.RowLength];
+                for (int i = 1; i < datas.Length; i++)
+                {
+                    MaterialData data = new MaterialData();
+                    data.Code = Convert.ToInt32(csvData[i, "CODE"]);
+                    data.Name = Convert.ToString(csvData[i, "NAME"]);
+
+                    datas[i] = data;
+                }
+                Set(datas);
+            }
+            #endregion
 #pragma warning restore CS0618 // 형식 또는 멤버는 사용되지 않습니다.
         }
 
@@ -63,6 +109,10 @@ namespace ProjectTrader.Datas
                 case ItemData[] itemDatas:
                     ItemDatas = itemDatas;
                     break;
+                case MaterialData[] materialDatas:
+                    MaterialDatas = materialDatas;
+                    break;
+
             }
         }
     }
