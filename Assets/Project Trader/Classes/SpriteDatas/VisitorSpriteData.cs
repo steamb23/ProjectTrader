86fdbcf1669b2d442109fbee71011b0a55731ed0
SteamB23 <steamb23@outlook.com>
2020-05-17 17:43:22 +0900

    손님 스프라디트 데이터 작성

--- Assets/Project Trader/Classes/SpriteDatas/VisitorSpriteData.cs
+++ Assets/Project Trader/Classes/SpriteDatas/VisitorSpriteData.cs
@@ -10,17 +10,17 @@ namespace ProjectTrader.SpriteDatas
     [CreateAssetMenu(fileName = "VisitorSpriteData", menuName = "스프라이트 데이터/손님 스프라이트 데이터")]
     public class VisitorSpriteData : ScriptableObject
     {
-        [Serializable]
-        public class VisitorSpriteDictionary : SerializableDictionary<string, ActionData> { }
+        //[Serializable]
+        //public class VisitorSpriteDictionary : SerializableDictionary<string, ActionData> { }
 
-        [SerializeField]
-        VisitorSpriteDictionary dictionary;
+        //[SerializeField]
+        //VisitorSpriteDictionary dictionary;
 
-        public VisitorSpriteDictionary Dictionary => dictionary;
+        //public VisitorSpriteDictionary Dictionary => dictionary;
 
         [Serializable]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235")]
-        public struct Data
+        public struct SpriteData
         {
             public Sprite[] Front;
             public Sprite[] Back;
@@ -28,12 +28,8 @@ namespace ProjectTrader.SpriteDatas
             public Sprite[] Right;
         }
 
-        [Serializable]
-        public struct ActionData
-        {
-            public Data Idle;
-            public Data Walk;
-            public Data Grab;
-        }
+        public SpriteData Idle;
+        public SpriteData Walk;
+        public SpriteData Grab;
     }
 }
