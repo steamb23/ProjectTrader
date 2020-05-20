626651c54b1132476cb1cb72f2fe0a008e41a59c
SteamB23 <steamb23@outlook.com>
2020-05-20 22:38:21 +0900

    GameDateTimeManager 정지 기능 추가

--- Assets/Project Trader/Scripts/GameDateTimeManager.cs
+++ Assets/Project Trader/Scripts/GameDateTimeManager.cs
@@ -44,11 +44,26 @@ class GameDateTimeManager : MonoBehaviour
     // 현실시간 5분 = 게임시간 1일
     // 현실시간 300초 = 게임시간 1440분 = 게임시간 86400초
     // 현실시간 1초 = 게임시간 288초
-    public float timeScale = 288;
 
-    public GameDateTimeInitData data;
-
+    [SerializeField]
+    private GameDateTimeInitData data;
+    /// <summary>
+    /// 게임내 시간 비율. 1초 = gameTimeScale
+    /// </summary>
+    [SerializeField]
+    private float gameTimeScale = 288f;
+    [SerializeField]
+    private bool isStopped = false;
+    [SerializeField]
     private GameDateTime gameDateTime;
+    [SerializeField]
+    private float timePart = 0f;
+
+    public bool IsStopped
+    {
+        get => isStopped;
+        set => isStopped = value;
+    }
 
     // 현재 게임내 시간을 가져옵니다.
     public GameDateTime GameDateTime
@@ -58,12 +73,30 @@ class GameDateTimeManager : MonoBehaviour
         {
             gameDateTime = value;
 
+#if UNITY_EDITOR
             // 초기화
             data = (GameDateTimeInitData)gameDateTime;
+#endif
         }
     }
 
-    public float timePart = 0;
+    /// <summary>
+    /// 현재 시간의 소수점부를 가져옵니다.
+    /// </summary>
+    public float TimePart
+    {
+        get => timePart;
+        set => timePart = value;
+    }
+    public void TimeStart()
+    {
+        isStopped = false;
+    }
+
+    public void TimeStop()
+    {
+        isStopped = true;
+    }
 
     /// <summary>
     /// 시간을 초기화합니다.
@@ -73,6 +106,7 @@ class GameDateTimeManager : MonoBehaviour
         gameDateTime = default;
         timePart = default;
         data = default;
+        isStopped = false;
     }
 
     private void Start()
@@ -82,20 +116,23 @@ class GameDateTimeManager : MonoBehaviour
 
     private void Update()
     {
-        // 업데이트
-        timePart += timeScale * Time.deltaTime;
+        if (!IsStopped)
+        {
+            // 업데이트
+            timePart += gameTimeScale * Time.deltaTime;
 
-        int gameTimeInt = (int)timePart;
-        // 소수점부 남기고 0으로 초기화
-        timePart -= gameTimeInt;
+            int gameTimeInt = (int)timePart;
+            // 소수점부 남기고 0으로 초기화
+            timePart -= gameTimeInt;
 
-        var gameDateTime = this.gameDateTime;
-        gameDateTime.AddSecond(gameTimeInt);
-        this.gameDateTime = gameDateTime;
+            var gameDateTime = this.gameDateTime;
+            gameDateTime.AddSecond(gameTimeInt);
+            this.gameDateTime = gameDateTime;
 
 #if UNITY_EDITOR
-        // 출력
-        data = (GameDateTimeInitData)gameDateTime;
+            // 출력
+            data = (GameDateTimeInitData)gameDateTime;
 #endif
+        }
     }
 }
