158863eb0a7482cc32c0da382dbe17a379d7a6d5
SteamB23 <steamb23@outlook.com>
2020-05-23 21:51:38 +0900

    EmployeeUiManager 구현

---
+++ Assets/Project Trader/Scripts/UI/EmployeeUiManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmployeeUiManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject background = null;
+
+    [SerializeField]
+    private GameObject employeeHireWindow = null;
+    [SerializeField]
+    private GameObject employeeAssignWindow = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (employeeHireWindow.activeSelf ||
+            employeeAssignWindow.activeSelf)
+        {
+            background.SetActive(true);
+        }
+        else
+        {
+            background.SetActive(false);
+        }
+    }
+
+    public void OpenEmployeeHire()
+    {
+        employeeHireWindow.SetActive(true);
+    }
+}
