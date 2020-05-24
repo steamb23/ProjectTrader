63114fed6452e9d06a664be422f4f3d4c2519bd9
SteamB23 <steamb23@outlook.com>
2020-05-25 00:07:23 +0900

    종료 버튼 구현

---
+++ Assets/Project Trader/Scripts/UI/ExitButton.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+class ExitButton : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject window;
+
+    public virtual void Click()
+    {
+        window.SetActive(false);
+    }
+}
