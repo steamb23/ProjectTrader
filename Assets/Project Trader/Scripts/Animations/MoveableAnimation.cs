8177d63eace223ba6ed64d3fe98134323adbb1c2
SteamB23 <steamb23@outlook.com>
2020-05-26 00:17:13 +0900

    손님 캐릭터 애니메이션 구현

--- Assets/Project Trader/Scripts/Animations/MoveableAnimation.cs
+++ Assets/Project Trader/Scripts/Animations/MoveableAnimation.cs
@@ -11,6 +11,20 @@ public abstract class MoveableAnimation : MonoBehaviour
     public AIPath aiPath;
     public FourDirection direction;
 
+    public enum AnimationState
+    {
+        Idle,
+        Walk,
+    }
+
+    public AnimationState state;
+
+    public float walkAnimationInterval = 0.2f;
+
+    public float time = 0;
+
+    public int frame = 0;
+
 
     private Vector3 previousPosition;
     // Use this for initialization
@@ -36,22 +50,86 @@ public abstract class MoveableAnimation : MonoBehaviour
         if (velocity.sqrMagnitude > aiPath.maxSpeed * aiPath.maxSpeed * 0.1f)
         {
             direction = Utils.VelocityToDirection(velocity);
+            state = AnimationState.Walk;
+
+            time += Time.deltaTime;
+            int whileCount = 0;
+            while (time > walkAnimationInterval)
+            {
+                time -= walkAnimationInterval;
+                frame++;
+                if(whileCount++ > 10)
+                {
+                    Debug.LogWarning("walkAnimationInterval이 현재 Time.deltaTime보다 값이 작아 무한 루프가 발생했습니다!");
+                    time = 0;
+                }
+            }
+        }
+        else
+        {
+            state = AnimationState.Idle;
+
+            frame = 0;
+            time = 0;
         }
 
-        switch (direction)
+        switch (state)
         {
-            case FourDirection.Up:
-                spriteRenderer.sprite = moveableSpriteData.Idle.Back[0];
+            case AnimationState.Idle:
+                IdleAnimation();
                 break;
-            case FourDirection.Down:
-                spriteRenderer.sprite = moveableSpriteData.Idle.Front[0];
+            case AnimationState.Walk:
+                WalkAnimaition();
                 break;
-            case FourDirection.Right:
-                spriteRenderer.sprite = moveableSpriteData.Idle.Right[0];
-                break;
-            case FourDirection.Left:
-                spriteRenderer.sprite = moveableSpriteData.Idle.Left[0];
-                break;
+        }
+
+
+        void IdleAnimation()
+        {
+            switch (direction)
+            {
+                case FourDirection.Up:
+                    spriteRenderer.sprite = moveableSpriteData.Idle.Back[0];
+                    break;
+                case FourDirection.Down:
+                    spriteRenderer.sprite = moveableSpriteData.Idle.Front[0];
+                    break;
+                case FourDirection.Right:
+                    spriteRenderer.sprite = moveableSpriteData.Idle.Right[0];
+                    break;
+                case FourDirection.Left:
+                    spriteRenderer.sprite = moveableSpriteData.Idle.Left[0];
+                    break;
+            }
+        }
+        void WalkAnimaition()
+        {
+            switch (direction)
+            {
+                case FourDirection.Up:
+                    if (moveableSpriteData.Walk.Back.Length > 0)
+                        spriteRenderer.sprite = moveableSpriteData.Walk.Back[frame % moveableSpriteData.Walk.Back.Length];
+                    else goto default;
+                    break;
+                case FourDirection.Down:
+                    if (moveableSpriteData.Walk.Front.Length > 0)
+                        spriteRenderer.sprite = moveableSpriteData.Walk.Front[frame % moveableSpriteData.Walk.Front.Length];
+                    else goto default;
+                    break;
+                case FourDirection.Right:
+                    if (moveableSpriteData.Walk.Right.Length > 0)
+                        spriteRenderer.sprite = moveableSpriteData.Walk.Right[frame % moveableSpriteData.Walk.Right.Length];
+                    else goto default;
+                    break;
+                case FourDirection.Left:
+                    if (moveableSpriteData.Walk.Left.Length > 0)
+                        spriteRenderer.sprite = moveableSpriteData.Walk.Left[frame % moveableSpriteData.Walk.Left.Length];
+                    else goto default;
+                    break;
+                default:
+                    IdleAnimation();
+                    break;
+            }
         }
     }
 }
