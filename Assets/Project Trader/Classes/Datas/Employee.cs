4912571a462880310dad1c76b5e462fb04b407b0
SteamB23 <steamb23@outlook.com>
2020-06-04 13:07:32 +0900

    EmployeeData 작업

---
+++ Assets/Project Trader/Classes/Datas/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTrader.Datas
+{
+    [Serializable]
+    struct Employee
+    {
+        /// <summary>
+        /// 아이템 코드
+        /// </summary>
+        [UnityEngine.SerializeField]
+        private int code;
+
+        public int Code
+        {
+            get => code;
+            set => code = value;
+        }
+
+        /// <summary>
+        /// 데이터 가져오기
+        /// </summary>
+        public ItemData GetData() => IngameDatabase.ItemDatas[code];
+    }
+}
---
+++ Assets/Project Trader/Classes/Datas/EmployeeData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProjectTrader.Datas
+{
+    [Serializable]
+    public class EmployeeData
+    {
+        public enum GenderType
+        {
+            Male,
+            Female
+        }
+
+        public int Code
+        {
+            get => this.code;
+            set => this.code = value;
+        }
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+        public GenderType Gender
+        {
+            get => this.gender;
+            set => this.gender = value;
+        }
+        public int Charisma
+        {
+            get => this.charisma;
+            set => this.charisma = value;
+        }
+        public int Intelligent
+        {
+            get => this.intelligent;
+            set => this.intelligent = value;
+        }
+        public int Dexterity
+        {
+            get => this.dexterity;
+            set => this.dexterity = value;
+        }
+        public int Tier
+        {
+            get => this.tier;
+            set => this.tier = value;
+        }
+        public int SpriteCode
+        {
+            get => this.spriteCode;
+            set => this.spriteCode = value;
+        }
+
+
+        #region 인스펙터 변수
+        [SerializeField] int code;
+        [SerializeField] string name;
+        [SerializeField] GenderType gender;
+        [SerializeField] int charisma;
+        [SerializeField] int intelligent;
+        [SerializeField] int dexterity;
+        [SerializeField] int tier;
+        [SerializeField] int spriteCode;
+        #endregion
+    }
+}
