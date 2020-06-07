e51f56643b470a39d9f8e8188586f5ba91bd2cb0
SteamB23 <steamb23@outlook.com>
2020-06-07 20:01:07 +0900

    직원 애니메이션 기능 구현

---
+++ Assets/Project Trader/Classes/SpriteDatas/EmployeeSpriteData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using ProjectTrader.SpriteDatas;
+
+namespace ProjectTrader.SpriteDatas
+{
+    [CreateAssetMenu(fileName = "EmployeeSpriteData", menuName = "스프라이트 데이터/직원 스프라이트 데이터")]
+    public class EmployeeSpriteData : MovableSpriteData
+    {
+        public Sprite[] Cleaning;
+    }
+}
---
+++ Assets/Project Trader/Scripts/Animations/EmployeeAnimation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using ProjectTrader.SpriteDatas;
+using ProjectTrader;
+
+public class EmployeeAnimation : MoveableAnimation
+{
+    public enum IdleStateType
+    {
+        Default,
+        Cleaning
+    }
+
+    [SerializeField] IdleStateType idleState;
+
+    public IdleStateType IdleState
+    {
+        get => this.idleState;
+        set => this.idleState = value;
+    }
+
+    protected override void IdleAnimation()
+    {
+        switch (idleState)
+        {
+            default:
+                base.IdleAnimation();
+                break;
+            case IdleStateType.Cleaning:
+                CleaningAnimation();
+                break;
+        }
+    }
+
+    protected virtual void CleaningAnimation()
+    {
+        for (int i = 0; i < animationDatas.Length; i++)
+        {
+            var spriteRenderer = animationDatas[i].spriteRenderer;
+            var movableSpriteData = animationDatas[i].movableSpriteData as EmployeeSpriteData;
+
+            if (movableSpriteData.Cleaning.Length > 0)
+                spriteRenderer.sprite = movableSpriteData.Cleaning[frame % movableSpriteData.Cleaning.Length];
+            else base.IdleAnimation();
+            break;
+        }
+    }
+}
