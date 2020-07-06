4a1fc58a16ee2599adab5978f85d7d62f3f0e6d5
SteamB23 <steamb23@outlook.com>
2020-07-06 23:22:26 +0900

    퀘스트 윈도우 탭 버튼 동작

---
+++ Assets/Project Trader/Scripts/UI/QuestWindow/QuestWindow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 퀘스트 윈도우 전체를 관리하는 비헤이비어입니다.
+/// </summary>
+public class QuestWindow : MonoBehaviour
+{
+    [SerializeField] GuideQuestPanel guideQuestPanel;
+    [SerializeField] DailyQuestPanel dailyQuestPanel;
+
+    private void OnEnable()
+    {
+        // 기본적으로 가이드 퀘스트를 띄움.
+        SwitchToGuideQuestPanel();
+    }
+
+    /// <summary>
+    /// 가이드 퀘스트 패널로 전환
+    /// </summary>
+    public void SwitchToGuideQuestPanel()
+    {
+        guideQuestPanel.gameObject.SetActive(true);
+        dailyQuestPanel.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 데일리 퀘스트 패널로 전환
+    /// </summary>
+    public void SwitchToDailyQuestPanel()
+    {
+        guideQuestPanel.gameObject.SetActive(false);
+        dailyQuestPanel.gameObject.SetActive(true);
+    }
+}
