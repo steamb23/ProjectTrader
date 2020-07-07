59d8a80a8918bb2c698d09e822f7e2a45ffb450f
SteamB23 <steamb23@outlook.com>
2020-07-07 15:36:26 +0900

    SubUI띄울때 백그라운드 표시하도록

---
+++ Assets/Project Trader/Scripts/UI/BackgroundScreenManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BackgroundScreenManager : MonoBehaviour
+{
+    [SerializeField] Image backgroundImage;
+    [SerializeField] GameObject[] targetObjects;
+
+    // 매 프레임마다 오브젝트 목록을 확인하고 켜진 오브젝트가 있으면 백그라운드도 같이 켭니다.
+    void LateUpdate()
+    {
+        bool isActive = false;
+
+        foreach (var targetObject in targetObjects)
+        {
+            if (targetObject.activeInHierarchy)
+            {
+                isActive = true;
+                break;
+            }
+        }
+
+        backgroundImage.gameObject.SetActive(isActive);
+    }
+}
