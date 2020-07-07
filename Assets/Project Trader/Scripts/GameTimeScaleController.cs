5b8967eada459a13e07055b07481206de7da3f2f
SteamB23 <steamb23@outlook.com>
2020-07-07 14:05:38 +0900

    게임 타임 스케일 컨트롤러 추가(임시)

---
+++ Assets/Project Trader/Scripts/GameTimeScaleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+class GameTimeScaleController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI buttonText;
+    [SerializeField] float timeScale = 1;
+
+    public void TouchSpeedButton()
+    {
+        switch (timeScale)
+        {
+            case 1:
+                timeScale = 2;
+                buttonText.text = "(2x) 더 빠르게";
+                break;
+            case 2:
+                timeScale = 3;
+                buttonText.text = "(3x) 느리게";
+                break;
+            default:
+                timeScale = 1;
+                buttonText.text = "(1x) 빠르게";
+                break;
+        }
+
+        Time.timeScale = timeScale;
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1;
+    }
+}
