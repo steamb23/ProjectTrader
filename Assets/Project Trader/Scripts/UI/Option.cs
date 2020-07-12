dd3fa50969e2848d1afe9307a981e054b69074e4
SteamB23 <steamb23@outlook.com>
2020-07-13 00:13:51 +0900

    오디오 볼륨 조정, 세팅 저장 구현

--- Assets/Project Trader/Scripts/UI/Option.cs
+++ Assets/Project Trader/Scripts/UI/Option.cs
@@ -1,34 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class Option : MonoBehaviour
 {
     [SerializeField] Slider bgm;
     [SerializeField] Slider effect;
+    [SerializeField] AudioMixer audioMixer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgm.value = PlayerPrefs.GetFloat("MusicVolume", AudioVolumeManager.DefaultMusicVolume);
+        effect.value = PlayerPrefs.GetFloat("EffectVolume", AudioVolumeManager.DefaultEffectVolume);
     }
 
     // Update is called once per frame
     void Update()
     {
+        BgmSlider();
         EffectSlider();
     }
 
     void BgmSlider()
     {
-
+        var volume = Mathf.Log10(bgm.value) * 20;
+        if (float.IsInfinity(volume))
+            volume = float.MinValue;
+        audioMixer.SetFloat("MusicVolume", volume);
+        PlayerPrefs.SetFloat("MusicVolume", bgm.value);
     }
 
     void EffectSlider()
     {
-        FindObjectOfType<SoundControl>().audioSource.volume = effect.value;
-        FindObjectOfType<SoundControl>().cleanAudioSource.volume = effect.value;
+        var volume = Mathf.Log10(effect.value) * 20;
+        if (float.IsInfinity(volume))
+            volume = float.MinValue;
+        audioMixer.SetFloat("EffectVolume", volume);
+        PlayerPrefs.SetFloat("EffectVolume", effect.value);
+        //FindObjectOfType<SoundControl>().audioSource.volume = effect.value;
+        //FindObjectOfType<SoundControl>().cleanAudioSource.volume = effect.value;
     }
 
     public void BgmPlusButton()
